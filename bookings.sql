fcc7da92a3272925dd2917c5d95ebe6cc88ca892
Saurabh-ch <43039641+Saurabh-ch@users.noreply.github.com>
2018-11-12 23:40:37 -0600

    Update bookings.sql

--- bookings.sql
+++ bookings.sql
@@ -2,6 +2,6 @@ CREATE TABLE bookings (
 `id` INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
 name varchar(20) null,
 email varchar(20) null,
-passengers varchar(2) null,
-departure date null
+room varchar(2) null,
+day date null
 )ENGINE=innodb;
